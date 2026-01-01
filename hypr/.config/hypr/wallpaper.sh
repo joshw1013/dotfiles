1e6f64df24e899d72fd6a5e6a7e4b64d9baa6e19
Josh Weingarden <joshwe@umich.edu>
2026-01-01 04:40:15 -0500

    Mostly breaking Hyprland changes, and additoinal tmux features

--- hypr/.config/hypr/wallpaper.sh
+++ hypr/.config/hypr/wallpaper.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
 WALLPAPER_DIR="$HOME/wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
-
+STATE_FILE="/tmp/current_wallpaper"
+CURRENT_WALL=$(cat "$STATE_FILE" 2>/dev/null)
 # Get a random wallpaper that is not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
-
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+# Save and apply
+echo "$WALLPAPER" > "$STATE_FILE"
+hyprctl hyprpaper wallpaper ", $WALLPAPER"
