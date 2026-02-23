0fe90ac59038ba3e50318850d1861fd68c619a29
Josh Weingarden <joshwe@umich.edu>
2026-02-22 23:29:07 -0500

    Can't get it working without bugs just going to turn it off

--- hypr/.config/hypr/scripts/scroll.sh
+++ hypr/.config/hypr/scripts/scroll.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # Usage: ./scroll.sh -1 (for up) or ./scroll.sh 1 (for down)
+hyprctl dispatch submap scroll
 
 PID_FILE="/tmp/ydotool_scroll.pid"
 
 # Nothing can run if we want to run something else
-./stop_scroll.sh
+$HOME/.config/hypr/scripts/stop_scroll.sh check
+
+if [ "$?" == "1" ]; then
+    exit 0
+fi
 
 # Save this script's exact Process ID to the tmp file
 echo $$ > "$PID_FILE"
 
 # Start the scroll loop
-while true; do
-    ydotool mousemove -w -x 0 -y "$1"
-    sleep 0.20
-done
+if [ "$1" == "-1" ]; then
+    # Scroll up
+    ydotool key 103:1
+else
+    # Scroll down
+    ydotool key 108:1
+    notify-send "scroll $1"
+fi
+exit 0
--- hypr/.config/hypr/scripts/stop_scroll.sh
+++ hypr/.config/hypr/scripts/stop_scroll.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# Usage: ./stop_scroll.sh -1 (for up) or ./stop_scroll.sh 1 (for down)
+# Usage: ./stop_scroll.sh
 
 PID_FILE="/tmp/ydotool_scroll.pid"
+hyprctl dispatch submap cursor
 
 # If the PID file exists, kill the exact process and remove the file
 if [ -f "$PID_FILE" ]; then
-    kill $(cat "$PID_FILE") 2>/dev/null
     rm "$PID_FILE"
+    # Kill both we don't really care about false positives
+    ydotool key 103:0
+    ydotool key 108:0
+    notify-send "Kill $1"
+    if [ "$1" == "check" ]; then
+        exit 1
+    fi
+    exit 0
 fi
